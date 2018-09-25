d3acd70207cebba65abfe1276a3c4bd614583ac3
Pablo Menéndez Suárez <pabloyo97@hotmail.com>
2018-09-25 23:49:05 +0200

    naves && ampliation

---
+++ JuegoNaves/src/modelos/Enemigo.js
@@ -0,0 +1,15 @@
+class Enemigo extends Modelo {
+
+    constructor(x, y) {
+        super(imagenes.enemigo, x, y)
+
+        this.vy = 0;
+        this.vx = 1;
+    }
+
+    actualizar (){
+        this.vx = -2;
+        this.x = this.x + this.vx;
+    }
+
+}
